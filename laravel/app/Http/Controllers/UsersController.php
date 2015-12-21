62661a2c581d89932bc76c702d3341b55ee1938b
shubhang-arora <sa287@snu.edu.in>
2015-12-21 12:45:04 +0530

    Small Change

--- laravel/app/Http/Controllers/UsersController.php
+++ laravel/app/Http/Controllers/UsersController.php
@@ -13,6 +13,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
         $this->middleware('isAdmin',
             ['only' =>  ['index']]);
     }
